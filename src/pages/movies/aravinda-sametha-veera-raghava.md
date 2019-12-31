14c1da5fd8cf6c7ebbc359308c28c8fa7c5e778a
kishoret99 <saikishoret99@gmail.com>
2019-12-31 12:31:40 +0530

    another wonderful commit

--- src/pages/movies/aravinda-sametha-veera-raghava.md
+++ src/pages/movies/aravinda-sametha-veera-raghava.md
@@ -3,9 +3,9 @@ templateKey: movie-details
 title: Aravinda Sametha Veera Raghava
 createdDate: 2019-11-20T13:40:07.172Z
 updatedDate: 2019-11-20T13:40:07.172Z
-director: people-1577772989835-6biNzn4E
+director: people-1577775651255-C0zxpETJ
 releasedDate: 2019-11-20T13:40:07.172Z
 artists:
-    - people-1577773062378-IBagaQc9
+    - artist: people-1577773062378-IBagaQc9
 ---
 
--- src/templates/movie-details.js
+++ src/templates/movie-details.js
@@ -1,48 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { kebabCase } from 'lodash'
-import Helmet from 'react-helmet'
-import { graphql, Link } from 'gatsby'
+import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
-import Content, { HTMLContent } from '../components/Content'
 
 export const MovieDetailsTemplate = ({
-  content,
-  contentComponent,
-  description,
-  tags,
-  title,
-  helmet,
+  title, 
 }) => {
-  const PostContent = contentComponent || Content
 
   return (
     <section className="section">
-      <h1>{'Movie detailss'}</h1>
-      {helmet || ''}
-      <div className="container content">
-        <div className="columns">
-          <div className="column is-10 is-offset-1">
-            <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
-              {title}
-            </h1>
-            <p>{description}</p>
-            <PostContent content={content} />
-            {tags && tags.length ? (
-              <div style={{ marginTop: `4rem` }}>
-                <h4>Tags</h4>
-                <ul className="taglist">
-                  {tags.map(tag => (
-                    <li key={tag + `tag`}>
-                      <Link to={`/tags/${kebabCase(tag)}/`}>{tag}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ) : null}
-          </div>
-        </div>
-      </div>
+      <h1 className="title is-size-2 has-text-weight-bold is-bold-light">
+        {title}
+      </h1>
     </section>
   )
 }
@@ -61,19 +30,6 @@ const MovieDetails = ({ data }) => {
   return (
     <Layout>
       <MovieDetailsTemplate
-        content={post.html}
-        contentComponent={HTMLContent}
-        description={post.frontmatter.description}
-        helmet={
-          <Helmet titleTemplate="%s | Blog">
-            <title>{`${post.frontmatter.title}`}</title>
-            <meta
-              name="description"
-              content={`${post.frontmatter.description}`}
-            />
-          </Helmet>
-        }
-        tags={post.frontmatter.tags}
         title={post.frontmatter.title}
       />
     </Layout>
